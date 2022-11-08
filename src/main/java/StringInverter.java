003ccc5af47186f393fba04fbaffa20e1ac2a945
Eduardo José Neto Oliveira <up202108843@up.pt>
2022-11-08 10:20:48 +0000

    String Inverter

---
+++ src/main/java/StringInverter.java
@@ -0,0 +1,22 @@
+import org.testng.annotations.Test;
+import static org.testng.AssertJUnit.assertEquals;
+import java.lang.StringBuilder;
+public class StringInverter implements StringTransformer{
+    String text_;
+    public StringInverter(){
+        text_ = "";
+    }
+    @Override
+    public void execute(StringDrink drink) {
+        StringBuilder a = new StringBuilder(drink.getText());
+        a.reverse();
+        drink.setText(a.toString());
+    }
+    @Test
+    public void stringInverter() {
+        StringDrink drink = new StringDrink("ABCD");
+        StringInverter si = new StringInverter();
+        si.execute(drink);
+        assertEquals("DCBA", drink.getText());
+    }
+}
